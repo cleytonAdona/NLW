80c4d9f293c8cf7308f2586c75321b0f40ad14a3
cleytonAdona <cleytonadona@outlook.com>
2020-08-12 17:29:05 -0300

    Interação com HTML no Backend

--- NLW/src/server.js
+++ NLW/src/server.js
@@ -1,13 +1,62 @@
+const proffys = [{
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "99988989",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "99988989",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Cleyton Adona",
+        avatar: "https://avatars0.githubusercontent.com/u/28422993?s=460&u=3bf523975a216fdf5edc86c7edec543ad33852ed&v=4",
+        whatsapp: "99988989",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Tecnologia",
+        cost: "23",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    }
+
+]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html", { proffys })
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
 const express = require('express')
 const server = express()
+
+//Configurar NunJucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
 server.use(express.static("public"))
-    .get("/", (req, res) => {
-        return res.sendFile(__dirname + "/views/index.html")
-    })
-    .get("/study", (req, res) => {
-        return res.sendFile(__dirname + "/views/study.html")
-    })
-    .get("/give-classes", (req, res) => {
-        return res.sendFile(__dirname + "/views/give-classes.html")
-    })
+    .get("/", pageLanding)
+    .get("/study", pageStudy)
+    .get("/give-classes", pageGiveClasses)
     .listen(5000)
